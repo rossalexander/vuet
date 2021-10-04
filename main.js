19ba6f6105f2fb9fb86a8f0c1105be62ee1f085e
Ross Alexander <rossaalexander@gmail.com>
2021-10-04 12:44:55 +0200

    Make task-list Vue component

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+Vue.component('task-list', {
+    template: `
+    <div>
+        <task v-for="task in tasks">{{ task.description }}</task>
+    </div>
+    `,
+
+    data() {
+        return {
+            tasks: [
+                {description: 'Go to the store.', complete: false},
+                {description: 'Go to the bank.', complete: false},
+                {description: 'Go to the house.', complete: false},
+                {description: 'Go to the movies.', complete: false},
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
-})
+});
 
 new Vue({
     el: '#root'
-})
+});
