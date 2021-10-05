0b04f543b09034655bb0ee173808bec1a237e43d
Ross Alexander <rossaalexander@gmail.com>
2021-10-05 10:44:13 +0200

    Make Vue modal with close event

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
-Vue.component('message', {
-    props: ['title', 'body'],
-    data() {
-        return {
-            isVisible: true
-        }
-    },
+Vue.component('modal', {
     template: `
-        <article class="message" v-show="isVisible">
-        <div class="message-header">
-            <p>{{title}}</p>
-            <button @click="isVisible = false" class="delete" aria-label="delete"></button>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <slot></slot>
+            </div>
         </div>
-        <div class="message-body">
-          {{body}}
-        </div>
-    </article>
-   `,
-    // methods: {
-    //     hideModal() {
-    //         this.isVisible = false
-    //     }
-    // }
+        <button @click="$emit('close')" class="modal-close is-large" aria-label="close"></button>
+    </div>`
 });
 
 new Vue({
-    el: '#root'
+    el: '#root',
+    data: {
+        showModal: false
+    }
 });
